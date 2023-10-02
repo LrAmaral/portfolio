17b28cfe213fc07b1d6c290ccedb41ae872174d0
Amaralcode19 <lucasamaralap@outlook.com>
2023-10-02 15:48:01 -0300

    :wrench: minor-changes

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,12 +4,12 @@ const Intro = () => {
   return (
     <div className='w-full h-screen flex flex-col justify-center items-center'>
         <div className="flex-col md:flex-row flex justify-center items-center gap-14">
-          <Image src={'/assets/profile.png'} alt="photo" width={300} height={300}/>
+          <Image src={'/assets/profile.png'} alt="photo" width={250} height={250}/>
           <div className="space-y-8">
             <div>
               <p className="text-neutral-400">Hello, I'm</p>
-              <p className="text-3xl font-bold">Lucas Amaral</p>
-              <p className="text-2xl font-bold text-neutral-400">Frontend Developer</p>
+              <p className="text-2xl md:text-3xl font-bold">Lucas Amaral</p>
+              <p className="text-xl md:text-2xl font-bold text-neutral-400">Frontend Developer</p>
             </div>
             <p className="font-bold md:text-3xl items-center">
                 I Bring Your Ideas To Life Through  <span className="text-alt">Programming</span>.
